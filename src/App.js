3ed34a2eba9f90c081d06d3f69d577a808f1495c
Hendrik de Lange <hendrik@alenticapitalfas.co.za>
2025-12-17 19:49:06 +0200

    Add footer with contact information to all pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './Abous';
 import Contact from './Contact';
 import Services from './Services';
 import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
@@ -16,6 +17,11 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/services" element={<Services />} />
       </Routes>
+      <footer style={{ backgroundColor: '#007bff', color: 'white', padding: '20px 0', marginTop: 'auto' }}>
+        <Container className="text-center">
+          <p style={{ margin: 0 }}>Contact: 076 721 7324 | Email: hendrik@alenticapitalfas.co.za</p>
+        </Container>
+      </footer>
     </div>
   );
 }
